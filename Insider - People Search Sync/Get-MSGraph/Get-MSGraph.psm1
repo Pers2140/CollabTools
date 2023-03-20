7a1ea732c6c16195ee5d6c3c0944bccab4fa8c48
Darius Persaud <darius.persaud@sri.com>
2023-03-20 14:24:58 -0700

    removed sensitive information

--- Insider - People Search Sync/Get-MSGraph/Get-MSGraph.psm1
+++ Insider - People Search Sync/Get-MSGraph/Get-MSGraph.psm1
@@ -1,27 +1,28 @@
 
 # Returns rest response as object
 function Get-UserToken {
-    # Set Params
-    # param(
-    #     [string] $client_id 
-    #     [string] $client_secret 
-    #     [string] $tenant_id 
-    # )
-        
-    $client_id = "89723cfa-0e2a-4663-b0cd-65b6cb94d5d1"
-    $client_secret = "7ER8Q~03ze-1eWCHKmGOjY7sAZ6RbIxoHYQygbLr"
-    $tenant_id = "8c0ee380-52d5-49f7-a4a2-34cf2be7b0b9"
 
     $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
     $headers.Add("Content-Type", "application/x-www-form-urlencoded")
     $headers.Add("Cookie", "buid=0.ATYAgOMOjNVS90mkojTPK-ewudfqWYwD1ydKnlXJagBUyNI2AAA.AQABAAEAAAD--DLA3VO7QrddgJg7WevrwzXRBf53TBob6qbZZNibQYJLRn1fWp_-0da2yf5resER9eV_jHbKJ8tJEk3Ek9mUZ7PuHWsVpMI8_7nrAuW7t0jbDkCVnEjdIegdNGEz8yYgAA; fpc=Al6oLhm8kABEh-fZyWWWvPOqTVxQAQAAAEZjd9sOAAAA; stsservicecookie=estsfd; x-ms-gateway-slice=estsfd")
 
-    $body = "grant_type=client_credentials&client_id=$client_id&client_secret=$client_secret&resource=https%3A%2F%2Fgraph.microsoft.com"
+    $body = "grant_type=client_credentials&client_id=$env:client_id&client_secret=$env:client_secret&resource=https%3A%2F%2Fgraph.microsoft.com"
 
-    $response = Invoke-RestMethod "https://login.microsoftonline.com/$tenant_id/oauth2/token" -Method 'POST' -Headers $headers -Body $body
+    $response = Invoke-RestMethod "https://login.microsoftonline.com/$env:tenant_id/oauth2/token" -Method 'POST' -Headers $headers -Body $body
     $response | convertTo-JSON | ConvertFrom-Json
 }
+# List drive IDs
+function Get-DriveIds {
 
+    $token = (Get-UserToken).access_token
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "Bearer $token")
+    $headers.Add("Content-Type", "text/plain")
+
+    $response = Invoke-RestMethod "https://graph.microsoft.com/v1.0/sites/$env:site_id/drives/" -Method 'GET' -Headers $headers 
+    $response | convertTo-JSON 
+    
+}
 # Upload file to Sharepoint - returns obj
 function Start-FileUpload {
     param(
@@ -39,10 +40,10 @@ function Start-FileUpload {
     if ($upload_dir){
         # write-host $file_path
             write-host "Uploading to directory ---> "${upload_dir}
-            Invoke-RestMethod "https://graph.microsoft.com/v1.0/sites/7733831a-48a3-4298-a44b-b87ca6c3b531/drives/b!GoMzd6NImEKkS7h8psO1Mal-S2J3q-1Jvy3dfVof4t2c7wenMrV6SoA4cZEqE54K/root:/${upload_dir}/${file_name}:/content" -Method 'PUT' -InFile $file_path -Headers $headers 
+            Invoke-RestMethod "https://graph.microsoft.com/v1.0/sites/$env:site_id/drives/$env:drive_id/root:/${upload_dir}/${file_name}:/content" -Method 'PUT' -InFile $file_path -Headers $headers 
         }else{
             write-host "Uploading file ---> "${file_name}
-            Invoke-RestMethod "https://graph.microsoft.com/v1.0/sites/7733831a-48a3-4298-a44b-b87ca6c3b531/drives/b!GoMzd6NImEKkS7h8psO1Mal-S2J3q-1Jvy3dfVof4t2c7wenMrV6SoA4cZEqE54K/root:/${file_name}:/content" -Method 'PUT' -InFile $file_path -Headers $headers 
+            Invoke-RestMethod "https://graph.microsoft.com/v1.0/sites/$env:site_id/drives/$env:drive_id/root:/${file_name}:/content" -Method 'PUT' -InFile $file_path -Headers $headers 
         }
     
     $response | convertTo-JSON | ConvertFrom-Json | select-object createdBy, createdDateTime, lastModifiedBy, lastModifiedDateTime  
--- Insider - People Search Sync/RunMe.ps1
+++ Insider - People Search Sync/RunMe.ps1
@@ -4,13 +4,20 @@ Import-Module ./Get-ADPhotos, ./Get-MSGraph, ./Update-StaffInfo
 # import-Module ActiveDirectoryTools 
 # Import-Module -Name ActiveDirectory
 
+$env:client_id = '89723cfa-0e2a-4663-b0cd-65b6cb94d5d1'
+$env:client_secret = '-Y58Q~2IH5rIcshgQQQ7w1q1ozjFi94w~-CTrb1n'
+$env:tenant_id = '8c0ee380-52d5-49f7-a4a2-34cf2be7b0b9'
+$env:site_id = '7733831a-48a3-4298-a44b-b87ca6c3b531'
+
+Get-DriveIds
+$env:drive_id = Read-Host -Prompt "`n`n What is your drive id? `n"
+
 write-host @"
 `n`n Choose a number `n`n 
 1. Update users information in insider 
 2. Update users photos
 3. Get new Bearer token `n`n 
 "@
-
 $Answer = Read-Host -Prompt"`n What would you like to do? `n"
 
 switch($Answer) {
@@ -33,4 +40,14 @@ switch($Answer) {
         $(Get-UserToken).access_token
         write-host "`n`n"
        }
+    4  {
+        write-host "`n`n Uploading file`n`n "
+        $(Get-UserToken).access_token
+        write-host "`n`n"
+       }
+    5  {
+        write-host "`n`n Uploading folder`n`n "
+        $(Get-UserToken).access_token
+        write-host "`n`n"
+        }
  }
